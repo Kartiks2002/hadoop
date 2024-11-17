41d899f7b503d5cff5e4d9fcb1a7315daf17499a
Kartiks2002 <colabkartik24@gmail.com>
2024-11-17 16:32:59 +0530

    Update IndexInverterJob.java

--- IndexInverter/IndexInverterJob.java
+++ IndexInverter/IndexInverterJob.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -14,6 +13,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * IndexInverterJob is a Hadoop MapReduce program that reads an input file where each line contains
@@ -21,6 +22,9 @@ import org.apache.hadoop.util.ToolRunner;
  * corresponding identifiers as values.
  */
 public class IndexInverterJob extends Configured implements Tool {
+
+    // Initialize the logger
+    private static final Logger logger = LogManager.getLogger(IndexInverterJob.class);
 
     /**
      * The Mapper class processes each line of the input file. It splits the line by commas and outputs
@@ -41,11 +45,14 @@ public class IndexInverterJob extends Configured implements Tool {
          */
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            logger.info("Processing line: " + value.toString());
+
             String[] words = value.toString().split(",");
             outputValue.set(words[0]); // First word is the identifier
             for (int i = 1; i < words.length; i++) {
                 outputKey.set(words[i]); // Each word is the output key
                 context.write(outputKey, outputValue); // Write word as key, identifier as value
+                logger.debug("Emitting key-value pair: (" + words[i] + ", " + words[0] + ")");
             }
         }
     }
@@ -69,6 +76,8 @@ public class IndexInverterJob extends Configured implements Tool {
          */
         @Override
         protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            logger.info("Reducing key: " + key.toString());
+
             StringBuilder builder = new StringBuilder();
             for (Text value : values) {
                 builder.append(value.toString()).append(",");
@@ -78,6 +87,7 @@ public class IndexInverterJob extends Configured implements Tool {
             }
             outputValue.set(builder.toString());
             context.write(key, outputValue); // Write word and concatenated identifiers
+            logger.debug("Reduced value for key " + key.toString() + ": " + builder.toString());
         }
     }
 
@@ -91,14 +101,23 @@ public class IndexInverterJob extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = super.getConf();
+        logger.info("Starting IndexInverterJob");
+
         Job job = Job.getInstance(conf, "IndexInverterJob");
         job.setJarByClass(IndexInverterJob.class);
 
         Path in = new Path(args[0]);
         Path out = new Path(args[1]);
 
+        // Log input and output paths
+        logger.info("Input Path: " + in.toString());
+        logger.info("Output Path: " + out.toString());
+
         // Delete output directory if it exists
-        out.getFileSystem(conf).delete(out, true);
+        if (out.getFileSystem(conf).exists(out)) {
+            logger.warn("Output path exists. Deleting: " + out.toString());
+            out.getFileSystem(conf).delete(out, true);
+        }
 
         // Set input and output paths
         FileInputFormat.setInputPaths(job, in);
@@ -119,6 +138,7 @@ public class IndexInverterJob extends Configured implements Tool {
         job.setOutputValueClass(Text.class);
 
         // Wait for the job to complete and return the result
+        logger.info("Waiting for job completion...");
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
@@ -130,10 +150,13 @@ public class IndexInverterJob extends Configured implements Tool {
     public static void main(String[] args) {
         int result;
         try {
+            logger.info("Running IndexInverterJob with arguments: " + String.join(", ", args));
             result = ToolRunner.run(new Configuration(), new IndexInverterJob(), args);
+            logger.info("Job completed with result code: " + result);
             System.exit(result);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Exception occurred during job execution", e);
+            System.exit(1);
         }
     }
 }
