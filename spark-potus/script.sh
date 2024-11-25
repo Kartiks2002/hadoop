502587dda5d722ed396449fce5132d85173530f0
Kartiks2002 <colabkartik24@gmail.com>
2024-11-26 01:03:37 +0530

    Update script.sh

--- spark-potus/script.sh
+++ spark-potus/script.sh
@@ -4,7 +4,11 @@
 # Created on: 25-11-2024 (23:37)
 # Last Modified on: 25-11-2024
 
+hdfs dfs -test -f whitehouse_visits.txt
 
-#hdfs dfs -copyFromLocal whitehouse_visits.txt
+if [ $? -eq 0 ];then
+        rm whitehouse_visits.txt
+        hdfs dfs -copyFromLocal whitehouse_visits.txt
+fi
 
 python extract_potus.py
